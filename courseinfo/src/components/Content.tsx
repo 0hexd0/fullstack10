855b4081ef4a36bc9fae6099e438f1282a6ae715
HeXudong <1514133606@qq.com>
2023-02-05 13:19:13 +0800

    exe9.15

--- courseinfo/src/components/Content.tsx
+++ courseinfo/src/components/Content.tsx
@@ -1,17 +1,122 @@
-import { CoursePart } from "../types";
+import {
+  CoursePart,
+  CoursePartBase,
+  CoursePartBaseNew,
+  CourseProjectPart,
+  CourseSubmissionPart,
+  CourseSpecialPart,
+} from "../types";
+
+const CoursePartBaseContent = ({
+  coursePart,
+}: {
+  coursePart: CoursePartBase;
+}) => {
+  const styleObj = {
+    fontWeight: 500,
+    fontSize: 18,
+    marginTop: "1em",
+  };
+  return (
+    <div style={styleObj}>
+      {coursePart.name} {coursePart.exerciseCount}
+    </div>
+  );
+};
+
+const CoursePartBaseNewContent = ({
+  coursePart,
+}: {
+  coursePart: CoursePartBaseNew;
+}) => {
+  return (
+    <>
+      <CoursePartBaseContent coursePart={coursePart} />
+      <div>{coursePart.description}</div>
+    </>
+  );
+};
+
+const CourseProjectPartContent = ({
+  coursePart,
+}: {
+  coursePart: CourseProjectPart;
+}) => {
+  return (
+    <>
+      <CoursePartBaseContent coursePart={coursePart} />
+      <div>project exercises {coursePart.groupProjectCount}</div>
+    </>
+  );
+};
+
+const CourseSubmissionPartContent = ({
+  coursePart,
+}: {
+  coursePart: CourseSubmissionPart;
+}) => {
+  return (
+    <>
+      <CoursePartBaseNewContent coursePart={coursePart} />
+      <div>submit to {coursePart.exerciseSubmissionLink}</div>
+    </>
+  );
+};
+
+const CourseSpecialPartContent = ({
+  coursePart,
+}: {
+  coursePart: CourseSpecialPart;
+}) => {
+  return (
+    <>
+      <CoursePartBaseNewContent coursePart={coursePart} />
+      <div>required skills: {coursePart.requirements.join(", ")}</div>
+    </>
+  );
+};
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return (
     <div>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      {courseParts.map((coursePart) => {
+        switch (coursePart.type) {
+          case "normal":
+            return (
+              <CoursePartBaseNewContent
+                key={coursePart.name}
+                coursePart={coursePart}
+              />
+            );
+          case "groupProject":
+            return (
+              <CourseProjectPartContent
+                key={coursePart.name}
+                coursePart={coursePart}
+              />
+            );
+          case "submission":
+            return (
+              <CourseSubmissionPartContent
+                key={coursePart.name}
+                coursePart={coursePart}
+              />
+            );
+          case "special":
+            return (
+              <CourseSpecialPartContent
+                key={coursePart.name}
+                coursePart={coursePart}
+              />
+            );
+          default:
+            throw new Error(
+              `Unhandled discriminated union member: ${JSON.stringify(
+                coursePart
+              )}`
+            );
+        }
+      })}
     </div>
   );
 };
--- courseinfo/src/types.ts
+++ courseinfo/src/types.ts
@@ -1,4 +1,34 @@
-export interface CoursePart {
+export interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+export interface CoursePartBaseNew extends CoursePartBase {
+  description: string;
+}
+
+export interface CourseNormalPart extends CoursePartBaseNew {
+  type: "normal";
+}
+
+export interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+export interface CourseSubmissionPart extends CoursePartBaseNew {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+export interface CourseSpecialPart extends CoursePartBaseNew {
+  type: "special";
+  requirements: string[];
+}
+
+export type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart;
